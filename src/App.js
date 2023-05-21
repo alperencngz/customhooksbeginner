fae6b42b8d7d536cb52e69a07c6022c44e8756e7
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-05-21 10:01:44 +0300

    Using custom hook method1
    I couldn't exactly figure out how to create it and use it but I think in the project where creating a custom hook have a goal, I will be able to do it

--- src/App.js
+++ src/App.js
@@ -1,59 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://customhooks-f2f4e-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const transformTasks = useCallback((tasksObject) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksObject) {
+      loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
     }
-    setIsLoading(false);
-  };
 
-  useEffect(() => {
-    fetchTasks();
+    setTasks(loadedTasks);
   }, []);
 
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
 
-  return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
-  );
-}
+    useEffect(() => {
+      fetchTasks({
+        url: "https://customhooks-f2f4e-default-rtdb.firebaseio.com/tasks.json"
+      });
+    }, [fetchTasks]);
+
+    const taskAddHandler = (task) => {
+      setTasks((prevTasks) => prevTasks.concat(task));
+    };
+
+    return (
+      <React.Fragment>
+        <NewTask onAddTask={taskAddHandler} />
+        <Tasks
+          items={tasks}
+          loading={isLoading}
+          error={error}
+          onFetch={fetchTasks}
+        />
+      </React.Fragment>
+    );
+  }
 
 export default App;
